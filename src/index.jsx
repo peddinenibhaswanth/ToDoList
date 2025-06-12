1b3ba807350f2d8a1e2c917e9d21b81dc79e5537
Bhaswanth <bhaswanthpeddineni@gmail.com>
2025-06-12 10:01:11 +0530

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+
+
+// use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
